bab6d7f8005f267f3b5592ea02f882233bc77978
Moritz Wallawitsch <wallawitsch@gmail.com>
2020-03-17 17:27:05 +0100

    besic dom interaction

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert("test")
+//Dom Interaction
+
+const getHistory = () => document.getElementById("history-value").innerText
+
+const printHistory = num =>
+  (document.getElementById("history-value").innerText = num)
+
+const getOutput = () => document.getElementById("output-value").innerText
+
+const printOutup = num =>
+  (document.getElementById("output-value").innerText = getNumFormat(num))
+
+const getNumFormat = num => {
+  let n = Number(num)
+  return n.toLocaleString("en")
+}
+
+const revNumFormat = num => Number(num.replace(/,/g, ""))
+
+//Calulations
